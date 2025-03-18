34da3fd1cf672e3e7f7960cb809893989807d7b3
Cristian Predusca <cristian.predusca@oveit.com>
2025-03-18 21:22:09 +0200

    18 martie final

--- saptamana-8/color-switcher/color-switcher.js
+++ saptamana-8/color-switcher/color-switcher.js
@@ -4,9 +4,12 @@
 // Regula: fiecare element selectat il bagam intr-o constanta
 // !IMPORTANT! cand selectorul nu este bun => constanta o sa fie egala cu null
 const sectionContainer = document.querySelector(".color-switcher-container");
-const switchColorButton = document.querySelector(
-  ".color-switcher-container button"
-);
+const previousColorButton = document.querySelector(".previous-button");
+const nextColorButton = document.querySelector(".next-button");
+const resetButton = document.querySelector(".reset-button");
+
+const displayedColorSpan = document.querySelector(".displayed-color");
+const numberOfClicksSpan = document.querySelector(".number-of-clicks");
 
 // 1) #AD636C
 // 2) #009B72
@@ -16,20 +19,55 @@ const switchColorButton = document.querySelector(
 
 const colors = ["#AD636C", "#009B72", "#6761AB", "#347E8D", "#798186"];
 //                  0          1           2         3          4
-let index = 1;
+let index = 0;
+let numberOfClicks = 0;
 
-const switchColor = () => {
-  console.log("start functie", index);
+const updateUserInterface = () => {
   sectionContainer.style.backgroundColor = colors[index];
+  displayedColorSpan.innerText = colors[index];
+  displayedColorSpan.style.color = colors[index];
+};
 
+// --- Previous ---
+const displayPreviousColor = () => {
+  index--;
+
+  if (index === -1) {
+    index = colors.length - 1;
+  }
+
+  updateUserInterface();
+};
+
+previousColorButton.addEventListener("click", displayPreviousColor);
+
+// --- Next ---
+const displayNextColor = () => {
   index++;
 
   if (index === colors.length) {
     index = 0;
   }
-  console.log("finish functie", index);
+
+  updateUserInterface();
+
+  numberOfClicks++;
+  numberOfClicksSpan.innerText = numberOfClicks;
 };
 
+nextColorButton.addEventListener("click", displayNextColor);
+
+const resetColor = () => {
+  index = 1;
+
+  sectionContainer.style.backgroundColor = colors[0];
+  displayedColorSpan.innerText = colors[0];
+  displayedColorSpan.style.color = colors[0];
+};
+
+resetButton.addEventListener("click", resetColor);
+
+// --- Varianta initiala ---
 // let counter = 0;
 
 // const switchColor = () => {
@@ -49,5 +87,3 @@ const switchColor = () => {
 //     sectionContainer.style.backgroundColor = "#AD636C";
 //   }
 // };
-
-switchColorButton.addEventListener("click", switchColor);
