35e409626210802e6f34bafc9bb7df607fe659f2
Cristian Predusca <cristian.predusca@oveit.com>
2025-07-01 21:08:53 +0300

    1 iulie 21:08

--- react/ecommerce/src/components/ShareProductButton.tsx
+++ react/ecommerce/src/components/ShareProductButton.tsx
@@ -1,34 +1,57 @@
-import { Share } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { isMobile } from "@/lib/is-mobile";
+import {
+  handleFacebookShare,
+  handleTwitterShare,
+  handleWhatsAppShare,
+} from "@/lib/social-share";
+import { Facebook, Share } from "lucide-react";
+import { useState } from "react";
+
+const shareData = {
+  title: "Check out this product!",
+  text: "Click the link below to access the product page and see many others.",
+  url: "https://developer.mozilla.org",
+};
 
 const ShareProductButton = () => {
+  let [isOpen, setIsOpen] = useState(false);
+
+  const handleShare = async () => {
+    const deviceIsMobile = isMobile();
+
+    // deviceIsMobile === false
+    if (!deviceIsMobile) {
+      setIsOpen(true);
+      return;
+    }
+
+    try {
+      await navigator.share(shareData);
+    } catch (err) {
+      setIsOpen(true);
+    }
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="secondary" size="icon">
-          <Share />
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Share link</DialogTitle>
-          <DialogDescription>
-            Anyone who has this link will be able to view this.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex items-center gap-2">
+    <>
+      <Button variant="secondary" size="icon" onClick={handleShare}>
+        <Share />
+      </Button>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Share link</DialogTitle>
+          </DialogHeader>
           <div className="grid flex-1 gap-2">
             <Label htmlFor="link" className="sr-only">
               Link
@@ -39,16 +62,25 @@ const ShareProductButton = () => {
               readOnly
             />
           </div>
-        </div>
-        <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-            <Button type="button" variant="secondary">
-              Close
+          <div className="flex items-center gap-2">
+            <hr className="flex-1" />
+            <p>OR</p>
+            <hr className="flex-1" />
+          </div>
+          <div className="flex justify-center gap-2">
+            <Button variant="ghost" onClick={() => handleFacebookShare("")}>
+              <Facebook />
             </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+            <Button variant="ghost" onClick={() => handleWhatsAppShare("")}>
+              <img src="" />
+            </Button>
+            <Button variant="ghost" onClick={() => handleTwitterShare("", "")}>
+              <img src="" />
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
--- react/ecommerce/src/lib/social-share.ts
+++ react/ecommerce/src/lib/social-share.ts
@@ -1,23 +1,23 @@
 export const handleFacebookShare = (currentPageURL: string) => {
   const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-    currentPageURL,
+    currentPageURL
   )}`;
-  window.open(shareUrl, '_blank', 'width=600,height=400');
+  window.open(shareUrl, "_blank", "width=600,height=400");
 };
 
 export const handleWhatsAppShare = (
   currentPageURL: string,
-  text: string = '',
+  text: string = ""
 ) => {
   const shareUrl = `https://wa.me/?text=${encodeURIComponent(
-    `${text} ${currentPageURL}`,
+    `${text} ${currentPageURL}`
   )}`;
-  window.open(shareUrl, '_blank', 'width=600,height=400');
+  window.open(shareUrl, "_blank", "width=600,height=400");
 };
 
 export const handleTwitterShare = (currentPageURL: string, text: string) => {
   const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-    currentPageURL,
+    currentPageURL
   )}&text=${encodeURIComponent(text)}`;
-  window.open(shareUrl, '_blank', 'width=600,height=400');
+  window.open(shareUrl, "_blank", "width=600,height=400");
 };
